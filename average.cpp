dc42636bcbcef297c76a588e326f2043c5d6a4e3
NostalgiaX-coder <iam01sweetz@gmail.com>
2026-03-06 15:27:12 +0700

    avg sc

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc, char *argv[]) {
+
+    if (argc == 1) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    double sum = 0;
+
+    for (int i = 1; i < argc; i++) {
+        sum += atof(argv[i]);
+    }
+
+    double avg = sum / (argc - 1);
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << argc - 1 << " numbers = " << avg << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
+}
